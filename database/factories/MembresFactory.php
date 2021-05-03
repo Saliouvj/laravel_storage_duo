7df9dc5d2349f6e7cc1a3d27948c21410ac85cba
Malo <ndekezi37@gmail.com>
2021-05-03 15:23:59 +0200

    debut exo

---
+++ database/factories/MembresFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Membres;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MembresFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Membres::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.index')
+@section('content')
+    <h1>home</h1>
+@endsection
